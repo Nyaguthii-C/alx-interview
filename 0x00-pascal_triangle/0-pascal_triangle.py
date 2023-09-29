32481c3ab1f8e016f2648f73c9e8d95bc1ea3c41
Nyaguthii-C <nyaguthiigc@gmail.com>
2023-09-29 00:35:57 +0000

    implement pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,24 +13,24 @@ def pascal_triangle(n):
         if not isinstance(n, int):
             raise TypeError("n must be an integer")
 
-
         if int(n) <= 0:
             return [[]]
-    
+
     #   initial row
         elif int(n) == 1:
             return [[1]]
-    
+
     #   Add row with coefficients depending on row number"""
         else:
             """ Default second row value"""
             Triangle = [[1], [1, 1]]
 
-            for i in range (2, n):
+            for i in range(2, n):
                 """calculate coefficients in  new row based on previous row"""
-                add_row = [1] + [Triangle[-1][j-1] + Triangle[-1][j] for j in range(1, i)] + [1]
+                add_row = [1] + [Triangle[-1][j-1] + Triangle[-1][j] /
+                                 for j in range(1, i)] + [1]
                 Triangle.append(add_row)
-            
+
             return Triangle
     # handle instance where n is passed as an non-integer value
     except TypeError as e:
