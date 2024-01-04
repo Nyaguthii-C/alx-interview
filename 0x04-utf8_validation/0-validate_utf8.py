71585ac7f08b54567fd5d6f78e99d747017ee3d5
Nyaguthii-C <nyaguthiigc@gmail.com>
2024-01-04 18:56:45 +0300

    determine if a given data set represents a valid UTF-8 encoding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""determine if given data set represents a valid UTF-8 encoding"""
+
+
+def validUTF8(data):
+    """convert data to bytes and check if valid utf-8"""
+    try:
+        bytes(data).decode('utf-8')
+        return True
+    except (UnicodeDecodeError, ValueError):
+        return False
